88201a8b10dbc5d3aecaac0dcc817f4fe31d5394
Priyanka Borse <priyanka.borse10@gmail.com>
2026-08-24 16:47:59 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,19 +1,10 @@
 terraform {
-  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 4.0"
     }
   }
-  cloud {
-    organization = "priLearn_Terraform"
-    workspaces {
-      name = "Terraform"
-    }
-  }
-}
-
-provider "azurerm" {
+ provider "azurerm" {
   features {}
 }
