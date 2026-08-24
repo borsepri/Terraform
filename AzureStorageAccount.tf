0668b46337b3e378bf61ab81b0220d6f91baeda0
Priyanka Borse <priyanka.borse10@gmail.com>
2026-08-24 17:46:33 +0200

    Update AzureStorageAccount.tf

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "demo" {
-  name     = "rg_terraform_pri"
+  name     = "rgterraformpri"
   location = "UK South"
 }
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "sa_terrform_pri"
+  name                     = "saterrformpri"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
